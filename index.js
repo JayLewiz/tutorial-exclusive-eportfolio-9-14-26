19ece5c64e2a00ac212e895b148690944075cdf2
JayLewiz <gony991@gmail.com>
2026-09-18 14:30:37 -0400

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// template_ygog665
+
+// service_rcxsp6h
+
+// cWo3rMKR9MIHVIVzV
+
+async function contact(event) {
+  event.preventDefault()
+  // await emailjs
+  //   .sendForm(
+  //     'service_rcxsp6h',
+  //     'template_ygog665',
+  //     event.target,
+  //     'cWo3rMKR9MIHVIVzV'
+  // )
+  setTimeout(() => {
+    console.log('this worked!')
+  }, 500)
+
+}
