9dc88f0c551fcc387831a2fc8da53cf3419de941
JayLewiz <gony991@gmail.com>
2026-09-19 12:04:33 -0400

    commit

--- index.js
+++ index.js
@@ -4,17 +4,31 @@
 
 // cWo3rMKR9MIHVIVzV
 
-async function contact(event) {
+function contact(event) {
   event.preventDefault()
-  // await emailjs
-  //   .sendForm(
-  //     'service_rcxsp6h',
-  //     'template_ygog665',
-  //     event.target,
-  //     'cWo3rMKR9MIHVIVzV'
-  // )
-  setTimeout(() => {
-    console.log('this worked!')
-  }, 500)
+  const loading = document.querySelector('.modal__overlay--loading')
+  const success = document.querySelector('.modal__overlay--success')
+  loading.classList += " modal__overlay--visible"
 
+
+  emailjs
+    .sendForm(
+      'service_rcxsp6h',
+      'template_ygog665',
+      event.target,
+      'cWo3rMKR9MIHVIVzV'
+  ).then(() => {
+    loading.classList.remove("modal__overlay--visible")
+    success.classList += " modal__overlay--visible"
+  }).catch(() => {
+    loading.classList.remove("modal__overlay--visible")
+    alert(
+      "The email service is temporarily unavailable. Please contact me directly on gony991@gmail.com"
+    )
+  })
+
+  setTimeout(() => {
+   
+    console.log('this worked!')
+  }, 1000)
 }
